0e90939b599158acaaf1d54a3b25d3dd47470dc2
Abdulloh Xo'jayev <xodjaevabdulloh54@gmail.com>
2024-04-08 12:05:18 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,5 @@
+let ism = prompt("Ismingiz nima:");
+let yosh = prompt("Yoshingizni kiriting");
+let manzil = prompt("Qaysi tumanda turasiz");
+let fan = prompt("Qaysi fanga qiziqasiz");
+alert(ism + " " + yosh + " " + manzil + " " + fan);
